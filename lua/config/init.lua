7da82c28a67a6939d172de303d2e9894ddfcac4d
Flureo <flureo@proton.me>
2026-03-03 16:40:50 -0500

    Update?

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,11 +1,11 @@
 require("config.options")
 require("config.lazy")
 
-vim.cmd[[colorscheme catppuccin-macchiato]]
+vim.cmd[[colorscheme tokyonight-night]]
 
-vim.cmd [[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
-]]
+-- vim.cmd [[
+--  highlight Normal guibg=none
+--  highlight NonText guibg=none
+--  highlight Normal ctermbg=none
+--  highlight NonText ctermbg=none
+-- ]]
